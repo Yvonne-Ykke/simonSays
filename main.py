165be46a9c546f4e845ec8ee6389d1faa376c314
Yvonne-Ykke <yvonne.koeroo@student.nhlstenden.com>
2024-12-05 22:04:31 +0100

    implemented curses library

--- main.py
+++ main.py
@@ -1,49 +1,70 @@
-#pip install keyboard
 #pip install numpy
+#pip install windows-curses
+
 import string
 import time
-import keyboard
+import curses
 import numpy as np
-
 
 alphabet = np.array(list(string.ascii_lowercase))
 
-def create_sequence():
-    current_sequence = np.random.choice(alphabet, size=5)
+def create_sequence(current_sequence):
+    current_sequence = np.append(current_sequence, np.random.choice(alphabet))
     return current_sequence
 
-def display_sequence(current_sequence):
-    print(current_sequence)
+def display_sequence(stdscr, current_sequence):
+    stdscr.clear()
+    for letter in current_sequence:
+        stdscr.addstr(0, 0, letter)
+        stdscr.refresh()
+        time.sleep(2)
+        stdscr.clear()
+        stdscr.refresh()
 
-def get_user_input():
-    time.sleep(5)
-    return 'letters'
+def get_user_input(stdscr, sequence_length):
+    user_input = []
+    stdscr.addstr(1, 0, 'Your turn! Press the correct keys!')
+    stdscr.refresh()
+    time.sleep(3)
 
+    for _ in range(sequence_length):
+        key = stdscr.getch()
+        user_input.append(chr(key))
+        stdscr.refresh()
+        time.sleep(0.5)
+    return user_input
 
 def check_user_input(current_sequence, user_input):
-    if user_input == current_sequence:
-        return True
+   if np.array_equal(current_sequence, user_input):
+       return True
+   else:
+       return False
+
+def play_round(stdscr, current_sequence):
+    new_sequence = create_sequence(current_sequence)
+    display_sequence(stdscr, new_sequence)
+    user_input = get_user_input(stdscr, len(new_sequence))
+
+    if check_user_input(new_sequence, user_input):
+        stdscr.addstr(2, 0, 'Next level!')
+        stdscr.refresh()
+        time.sleep(1)
+        play_round(stdscr, new_sequence)
     else:
-        return False
+        stdscr.addstr('Sorry, you lose!')
+        stdscr.refresh()
+        time.sleep(3)
 
+def start_game(stdscr):
+    stdscr.clear()
+    stdscr.addstr(0, 0, 'Welcome to Simon Says!')
+    stdscr.refresh()
+    time.sleep(2)
 
-def start_game(difficulty=None):
-    print('Welcome to Simon Says!')
+    current_sequence = np.empty(0, dtype=str)
+    play_round(stdscr, current_sequence)
 
-    current_sequence = create_sequence()
-    display_sequence(current_sequence)
-    user_input = get_user_input()
-
-    if check_user_input(current_sequence, user_input):
-        print('Next level!')
-        difficulty +=1
-        #ToDo endless loop here
-    else:
-        print('Sorry, you lose!')
-
-    print('Press esc to exit')
-    keyboard.wait('esc')
 
 if __name__ == '__main__':
-    start_game()
+    curses.wrapper(start_game)
 
